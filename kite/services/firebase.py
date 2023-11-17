2e6ac23a7bba52f7afe564dbf4379720124c88ee
BurhanArif4211 <116505131+BurhanArif4211@users.noreply.github.com>
2023-11-17 22:31:00 +0500

    Update firebase.py
    removed sensitive data

--- kite/services/firebase.py
+++ kite/services/firebase.py
@@ -1,13 +1,7 @@
 import firebase, base64
 from ErrorCodes import STATUS_CODES 
 fireConfig = {
-    "apiKey": "AIzaSyDHP6GQhjz0uFtrnuFllumERl-HmGSA9kA",
-    "authDomain": "potfolio-492d3.firebaseapp.com",
-    "databaseURL": "https://potfolio-492d3-default-rtdb.asia-southeast1.firebasedatabase.app",
-    "projectId": "potfolio-492d3",
-    "storageBucket": "potfolio-492d3.appspot.com",
-    "messagingSenderId": "1041514184898",
-    "appId": "1:1041514184898:web:d02a11cc9eac9cd6d7bb5b"
+....
 }
 
 fireApp = firebase.initialize_app(fireConfig)
