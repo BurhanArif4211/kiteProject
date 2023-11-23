11fda35d15be666bb81b40314f7a4aa2d26dd5e2
BurhanArif4211 <masterrecon777@gmail.com>
2023-11-23 22:21:35 +0500

    fixed credentials

--- kite/services/firebase.py
+++ kite/services/firebase.py
@@ -5,16 +5,9 @@ import firebase_admin
 from firebase_admin import credentials, firestore,storage
 
 fireConfig = {
-"apiKey" : "AIzaSyDHP6GQhjz0uFtrnuFllumERl-HmGSA9kA",
-    "authDomain" : "potfolio-492d3.firebaseapp.com",
-    "databaseURL" : "https://potfolio-492d3-default-rtdb.asia-southeast1.firebasedatabase.app",
-    "projectId" : "potfolio-492d3",
-    "storageBucket" : "potfolio-492d3.appspot.com",
-    "messagingSenderId" : "1041514184898",
-    "appId" : "1:1041514184898:web:d02a11cc9eac9cd6d7bb5b"
+## put creds here!!!!
 }
-
-firebase_admin.initialize_app(credentials.Certificate('../../../Downloads/potfolio-492d3-firebase-adminsdk-c1s9v-a2c77c2b1c.json'),{'storageBucket': 'potfolio-492d3.appspot.com'})
+firebase_admin.initialize_app(credentials.Certificate('Put cred here!!'),{'storageBucket': 'potfolio-492d3.appspot.com'})
 # # firestore Quota: 1GB
 store = firestore.client()
 # storage=storage
